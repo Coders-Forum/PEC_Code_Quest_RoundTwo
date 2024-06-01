11a168bccf5917d5076d07175e942e8bfa537b7b
Coders Forum <139980007+Coders-Forum@users.noreply.github.com>
2024-06-01 22:24:37 +0530

    Create FormattingIssues-CF.py

---
+++ src/FormattingIssues-CF.py
@@ -0,0 +1,7 @@
+content = """Hello Coders Forum! %% && \\
+Happy \\new year !
+\\\\\\
+"Double-quotes" 'Single-quotes' \"\"\"Triple-double-quotes\"\"\" '''Triple-single-quotes'''
+Bye bye!
+"""
+print(content)
