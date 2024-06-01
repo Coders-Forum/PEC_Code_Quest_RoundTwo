01416d6217408f371c9b685dc6359b64e4cf252a
Coders Forum <139980007+Coders-Forum@users.noreply.github.com>
2024-06-01 22:51:57 +0530

    Create IsItPossible-CF.py

---
+++ src/IsItPossible-CF.py
@@ -0,0 +1,9 @@
+m,n=map(int,input().split())
+p,q=map(int,input().split())
+
+'''Note:Number of Column in the first matrix must be equal to Number of Rows in the second matrix'''
+
+if(p==n):
+    print("Yes")
+else:
+    print("No")
