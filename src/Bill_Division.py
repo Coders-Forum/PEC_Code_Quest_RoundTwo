966683cd1080a4a622efa66a87b5c7d1e4cb287a
Coders Forum <139980007+Coders-Forum@users.noreply.github.com>
2024-06-01 22:17:54 +0530

    Delete src directory

--- src/Bill_Division.py
+++
@@ -1,9 +0,0 @@
-def bonAppetit(bill, k, b):
-    half= sum(bill) / 2
-    bill.pop(k)
-    anna= sum(bill) / 2
-    
-    if b == anna:
-        print('Bon Appetit')
-    else:
-        print(int(half - anna))
