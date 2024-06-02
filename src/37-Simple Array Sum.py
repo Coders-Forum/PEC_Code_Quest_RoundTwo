b3ee2b0785a1e9236ce3cd086854f171ab8acee1
Coders Forum <139980007+Coders-Forum@users.noreply.github.com>
2024-06-02 11:19:31 +0530

    Create 37-Simple Array Sum.py

---
+++ src/37-Simple Array Sum.py
@@ -0,0 +1,14 @@
+#Using Built-in Function
+def simpleArraySum(ar):
+    return(sum(ar))
+
+#OR
+
+#By iterate over the array
+''' 
+def simpleArraySum(ar):
+    sum1=0
+    for i in ar:
+      sum1+=i
+  return sum1
+'''
